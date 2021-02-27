9c1fe93715f152618b85d114f1e5ebc35223af86
lotsofluck <anitabangali@yahoo.com>
2021-02-26 22:29:08 -0600

    Insert function

--- HashTable.py
+++ HashTable.py
@@ -43,11 +43,17 @@ class HashTable:
   # Should insert a key value pair into the hash table, where the key is the word and the value is a counter for the number of times the word appeared. When inserting a new word in the hash table, be sure to check if there is a Node with the same key in the table already.
 
   def insert(self, key, value):
-    pass
+    
+    new_pair = (key,value)
+    index = self.hash_func(key)
+    ll = self.arr[index]
 
-
-
-
+    if ll.find(key) == -1:
+      ll.append(new_pair)
+    
+    else:
+      ll.update(key)
+      
   # 4️⃣ TODO: Complete the print_key_values method.
 
   # Traverse through the every Linked List in the table and print the key value pairs.
